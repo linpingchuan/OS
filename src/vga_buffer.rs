a68e33bb8f5e60bfc8653f57af2d3f017d8abdad
lin pingchuan <paul_lin@foxmail.com>
2019-09-18 17:08:40 +0800

    增加writer宏

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,7 @@
+use volatile::Volatile;
+use core::fmt;
+use core::fmt::Write;
+
 #[allow(dead_code)]
 #[derive(Debug,Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -41,7 +45,7 @@ const BUFFER_WIDTH:usize=80;
 
 #[repr(transparent)]
 struct Buffer{
-    chars:[[ScreenChar;BUFFER_WIDTH];BUFFER_HEIGHT]
+    chars:[[Volatile<ScreenChar>;BUFFER_WIDTH];BUFFER_HEIGHT]
 }
 
 pub struct Writer{
@@ -62,10 +66,10 @@ impl Writer{
                 let col=self.column_position;
 
                 let color_code=self.color_code;
-                self.buffer.chars[row][col]=ScreenChar{
+                self.buffer.chars[row][col].write(ScreenChar{
                     ascii_character:byte,
                     color_code,
-                };
+                });
                 self.column_position+=1;
             }
         }
@@ -85,6 +89,14 @@ impl Writer{
 
 }
 
+impl fmt::Write for Writer{
+    fn write_str(&mut self,s:&str)->fmt::Result{
+        self.write_string(s);
+        Ok(())
+    }
+
+}
+
 pub fn print_something(){
         let mut writer=Writer{
             column_position:0,
@@ -95,4 +107,5 @@ pub fn print_something(){
         writer.write_byte(b'H');
         writer.write_string("ello ");
         writer.write_string("这是你想要的吗，说好不哭的");
+        write!(writer,"The number are {} and {}",42,1.0/3.0).unwrap();
     }
