6ff09425161b51f839fcad3433a4eecac0b27c79
lin pingchuan <paul_lin@foxmail.com>
2019-09-23 12:25:53 +0800

    增加自定义测试框架

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
 use core::panic::PanicInfo;
 mod vga_buffer;
 
@@ -9,6 +12,16 @@ pub extern "C" fn _start() -> ! {
 
     vga_buffer::print_something();
     loop {}
+}
+
+
+
+#[cfg(test)]
+fn test_runner(tests:&[&dyn Fn()]){
+    println!("Running {} tests",tests.len());
+    for test in tests{
+        test();
+    }
 }
 
 #[panic_handler]
