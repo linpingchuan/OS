161a22bffa89d93ce59f677219fc42b786a9723b
lin pingchuan <paul_lin@foxmail.com>
2019-09-23 15:42:39 +0800

    增加自动化测试框架退出

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
-#![reexport_test_harness_main="test_main"]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello 林平川 {}","!");
+    println!("Hello 林平川 {}", "!");
 
     vga_buffer::print_something();
 
@@ -19,23 +19,40 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code:QemuExitCode){
+    use x86_64::instructions::port::Port;
+
+    unsafe{
+        let mut port=Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[cfg(test)]
-fn test_runner(tests:&[&dyn Fn()]){
-    println!("Running {} tests",tests.len());
-    for test in tests{
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}",info);
+    println!("{}", info);
     loop {}
 }
 
 #[test_case]
-fn trivial_assertion(){
+fn trivial_assertion() {
     print!("trivial assertion...");
-    assert_eq!(1,1);
+    assert_eq!(1, 1);
     println!("[ok]");
 }
